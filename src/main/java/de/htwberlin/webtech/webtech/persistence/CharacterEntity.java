17adbd7203cbeeb84aa652d7e85503b01a8379ff
Shevin Walo <107920728+WaloShevin@users.noreply.github.com>
2024-06-23 16:52:06 +0200

    character Entity update

--- src/main/java/de/htwberlin/webtech/webtech/persistence/CharacterEntity.java
+++ src/main/java/de/htwberlin/webtech/webtech/persistence/CharacterEntity.java
@@ -1,6 +1,7 @@
 package de.htwberlin.webtech.webtech.persistence;
 
 import jakarta.persistence.*;
+import org.springframework.lang.NonNull;
 
 import javax.annotation.processing.Generated;
 
@@ -9,19 +10,27 @@ public class CharacterEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
     private String name;
     private int age;
 
     @Enumerated(value = EnumType.STRING)
     private Gender gender;
+
+    @Column(nullable = false)
+    @NonNull
     private String species;
+    @Column(nullable = false)
+    @NonNull
     private String significantSkill;
+    @Column(nullable = false)
+    @NonNull
     private String weakness;
 
+    @Column(nullable = false, columnDefinition = "TEXT")
+    @NonNull
     private String story;
-    
+
 
 
     public CharacterEntity( Long id, String name, int age, Gender gender, String species,String significantSkill, String weakness, String story ) {
@@ -73,37 +82,39 @@ public class CharacterEntity {
         this.gender = gender;
     }
 
+    @NonNull
     public String getSpecies() {
         return species;
     }
 
-    public void setSpecies(String species) {
+    public void setSpecies(@NonNull String species) {
         this.species = species;
     }
 
+    @NonNull
     public String getSignificantSkill() {
         return significantSkill;
     }
 
-    public void setSignificantSkill(String significantSkill) {
+    public void setSignificantSkill(@NonNull String significantSkill) {
         this.significantSkill = significantSkill;
     }
 
+    @NonNull
     public String getWeakness() {
         return weakness;
     }
 
-    public void setWeakness(String weakness) {
+    public void setWeakness(@NonNull String weakness) {
         this.weakness = weakness;
     }
 
+    @NonNull
     public String getStory() {
         return story;
     }
 
-    public void setStory(String story) {
+    public void setStory(@NonNull String story) {
         this.story = story;
     }
-
-
 }
